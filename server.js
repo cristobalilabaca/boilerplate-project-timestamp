08476a28e07ed0f787d1630ef67eda9ffc48e7be
Cristobal ilabaca <cilabaca1@uc.cl>
2021-10-08 13:58:13 -0300

    finished timestamp service

--- server.js
+++ server.js
@@ -25,13 +25,32 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api", function(req, res) {
+  console.log("hi")
   now = new Date;
   res.json({utc: now, unix: Date.now()});
 })
 
+app.get("/api/:date", function(req, res) {
+  try {
+    let date;
+    if (!isNaN(req.params.date)) {
+      date = new Date(parseInt(req.params.date));
+    } else {
+      date = new Date(req.params.date);
+    }
+    if (date == "Invalid Date") {
+      res.json({ error: "Invalid Date"});
+    } else {
+      res.json({utc: date.toUTCString(), unix: date.getTime()});
+    };
+  } catch {
+    res.json({ error : "error" });
+  };
+});
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
